4070e632e44df96249b28b379630eea647dcd928
Rups <rups_54@hotmail.com>
2022-06-05 10:49:42 +0100

    convert to OO

--- main.py
+++ main.py
@@ -13,100 +13,91 @@ import argparse
 #
 # pd.set_option('display.max_columns', None)
 s = HTMLSession()
-dealslist = []
-#
-#
-url = 'https://www.amazon.co.uk/s?k=bin&i=black-friday'
-#
-def getdata(url):
-    r = s.get(url)
-    r.html.render(sleep=1)
-    soup = BeautifulSoup(r.html.html, 'html.parser')
-    return soup
 
-def getdeals(soup):
-     products = soup.find_all('div', {'data-component-type': 's-search-result'})
-     print(len(products))
-     for item in products:
-      title = item.find('a', {'class': 'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal'}).text.strip()
-      short_title = item.find('a', {'class': 'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal'}).text.strip()[:30]
-      try:
-       saleprice = float(item.find_all('span', {'class': 'a-offscreen'})[0].text.replace('£','').replace(',','').strip())
-       oldprice = float(item.find_all('span', {'class': 'a-offscreen'})[1].text.replace('£','').replace(',','').strip())
-      except:
-       oldprice = float(item.find('span', {'class': 'a-offscreen'}).text.replace('£','').replace(',','').strip())
-      try:
-       reviews = item.find('span', {'class': 'a-size-base s-underline-text'}).text.strip()
-      except:
-       reviews = 0
-      try:
-       stars = item.find('a', {'class': 'a-popover-trigger a-declarative'}).text.strip()
-      except:
-       stars = 'No ratings'
+url = 'https://www.amazon.co.uk/s?k=bin'
 
-      saleitem = {
-      # 'title': title,
-      'short_title': short_title,
-      'saleprice': saleprice,
-      'oldprice': oldprice,
-      'reviews': reviews,
-      'stars': stars
-      }
-      dealslist.append(saleitem)
+class Session(object):
+    def __init__(self):
+        self.url = url
+        self.getdata(url)
+        self.dealslist = []
 
-     print(dealslist)
+    def getdata(self, url):
+        try:
+            r = s.get(self.url)
+            r.html.render(sleep=1)
+            soup = BeautifulSoup(r.html.html, 'html.parser')
+        except:
+            print('Something went wrong with loading the page' + e)
+        self.getdeals(soup)
 
-     return dealslist
+    def checkpage(self, soup):
+        npf = soup.find('div', {'class': 'a-section a-text-center s-pagination-container'})
+        if npf is None:
+            print ('this is the only page')
+            return
+        else:
+            print('there is more than one page')
+            print('checking for the next page button')
+            nextbut = soup.find('a', {'class': 's-pagination-item s-pagination-next s-pagination-button s-pagination-separator'})
+            if nextbut is not None:
+                print('whoop next button is avail')
+                self.findnexturl(nextbut, True)
+            elif nextbut is None:
+                print('next button isnt available')
+            else:
+                print('not sure how i got here')
 
-def nextpage(soup):
-    print('hello im here')
-    pages = soup.find('div', {'class': 'a-section a-text-center s-pagination-container'})
-
-    while not pages.find('span', {'class': 's-pagination-item s-pagination-next s-pagination-disabled '}):
-        print('next page available')
-        url = 'https://www.amazon.co.uk'+ str(pages.find('a', {'class': 's-pagination-item s-pagination-next s-pagination-button s-pagination-separator'})['href'])
+    def findnexturl(self, nextbut, goto=False):
+        urlpart = nextbut['href']
+        print(urlpart)
+        self.url = 'https://www.amazon.co.uk' + urlpart
         print(url)
-        getdata(url)
-        pages = soup.find('div', {'class': 'a-section a-text-center s-pagination-container'})
-        print('looping')
+        if goto:
+            self.getdata(url)
 
-    return
+    def getdeals(self, soup):
+        print('getting deals')
+        products = soup.find_all('div', {'data-component-type': 's-search-result'})
+        print(len(products))
+        for item in products:
+            title = item.find('a', {'class': 'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal'}).text.strip()
+            short_title = item.find('a', {'class': 'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal'}).text.strip()[:30]
+            try:
+                saleprice = float(item.find_all('span', {'class': 'a-offscreen'})[0].text.replace('£','').replace(',','').strip())
+                oldprice = float(item.find_all('span', {'class': 'a-offscreen'})[1].text.replace('£','').replace(',','').strip())
+            except:
+                oldprice = float(item.find('span', {'class': 'a-offscreen'}).text.replace('£','').replace(',','').strip())
+            try:
+                reviews = item.find('span', {'class': 'a-size-base s-underline-text'}).text.strip()
+            except:
+                reviews = 0
+            try:
+                stars = item.find('a', {'class': 'a-popover-trigger a-declarative'}).text.strip()
+            except:
+                stars = 'No ratings'
+        saleitem = {
+            'title': title,
+            'short_title': short_title,
+            'saleprice': saleprice,
+            'oldprice': oldprice,
+            'reviews': reviews,
+            'stars': stars
+                    }
+        self.dealslist.append(saleitem)
+        print(dealslist)
+        return
 
-t = getdata(url)
-data = getdeals(t)
-df = pd.DataFrame(data)
-df['percentoff'] = 100 - ((df.saleprice / df.oldprice) * 100)
-df = df.sort_values(by=['percentoff'], ascending=False)
-print(df.to_string())
 
-q = nextpage(t)
+bot = Session()
 
-# df.to_csv(searchterm + '-bfdeals.csv', index=False)
-# print('Fin.')
-# print('hello')
-
-#     for item in products:
-#         title = item.find('a', {'class': 'a-link-normal a-text-normal'}).text.strip()
-#         short_title = item.find('a', {'class': 'a-link-normal a-text-normal'}).text.strip()[:25]
-#         link = item.find('a', {'class': 'a-link-normal a-text-normal'})['href']
-#         try:
-#             saleprice = float(item.find_all('span', {'class': 'a-offscreen'})[0].text.replace('£','').replace(',','').strip())
-#             oldprice = float(item.find_all('span', {'class': 'a-offscreen'})[1].text.replace('£','').replace(',','').strip())
-#         except:
-#             oldprice = float(item.find('span', {'class': 'a-offscreen'}).text.replace('£','').replace(',','').strip())
-#         try:
-#             reviews = float(item.find('span', {'class': 'a-size-base'}).text.strip())
-#         except:
-#             reviews = 0
+# t = getdata(url)
+# data = getdeals(t)
+# df = pd.DataFrame(data)
+# df['percentoff'] = 100 - ((df.saleprice / df.oldprice) * 100)
+# df = df.sort_values(by=['percentoff'], ascending=False)
+# print(df.to_string())
 #
-#         saleitem = {
-#             'title': title,
-#             'short_title': short_title,
-#             'link': link,
-#             'saleprice': saleprice,
-#             'oldprice': oldprice,
-#             'reviews': reviews
-#             }
-#         dealslist.append(saleitem)
-#     return doing a test
+# q = nextpage(t)
+
 
